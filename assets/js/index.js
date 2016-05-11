aa85370f715ad4b38ee073f61c2178f16d4739f0
尹曰 <junjun.zjj@alipay.com>
2016-05-11 22:19:53 +0800

    Support smooth scroll

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+(function() {
+
+  var doc = document.documentElement
+  var body = document.body
+  var sidebar = document.getElementById('sidebar')
+
+  // listen for scroll event to do positioning
+  window.addEventListener('scroll', toggleSidebarClass, false)
+  window.addEventListener('resize', toggleSidebarClass, false)
+
+  // init smooth scroll
+  smoothScroll.init({
+    speed: 400,
+    offset: window.innerWidth > 720
+      ? 40
+      : 58
+  })
+
+  function toggleSidebarClass() {
+    if (!sidebar || !sidebar.classList) {
+      return
+    }
+
+    var top = doc && doc.scrollTop || body.scrollTop
+
+    if (top > 70) {
+      sidebar.classList.add('fixed')
+    } else {
+      sidebar.classList.remove('fixed')
+    }
+  }
+
+})()
